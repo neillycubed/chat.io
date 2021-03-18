03763fdd1024636601b71042819c4d7a0fef6d66
neillycubed <neillybusiness3@gmail.com>
2021-03-17 19:20:34 -0700

    add messaging components and styling changes

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -55,7 +55,11 @@ const Chat = ({location}) => {
     return (
         <div className='outerContainer'>
             <div className='container'>
+                <InfoBar room={room}/>
+                <Messages messages={messages} name={name} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
+            <TextContainer users={users} />
         </div>
     )
 };
